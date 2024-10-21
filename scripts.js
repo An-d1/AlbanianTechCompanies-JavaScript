c35ff0671f0babbcde3d9db0e05dad8e98a27eb5
Andis <andisbara@gmail.com>
2024-10-21 18:49:21 +0200

    final update

---
+++ scripts.js
@@ -0,0 +1,34 @@
+import { companiesData } from "./data.js";
+
+let companiesHTML = '';
+
+companiesData.forEach((company) => {
+  companiesHTML += `
+  <div class="col-12 col-sm-6 col-md-3">
+          <div class="flex-container">
+          <a class="websiteLink" href="${company.website}" target = _blank>
+            <div class="title">
+              "${company.titleCompany}"
+            </div>
+
+            <div class="mainImg">
+              <img src="images/1721802899_0a08ccaa9a91933a0b20.jpg">
+            </div>
+
+            <div class="category">
+              Description: Software company
+            </div>
+            </a>
+
+            <a href="${company.link}" target = _blank>
+            <button class="location">
+              open in google maps
+            </button>
+            </a>
+            
+          </div>
+        </div>
+    `;
+});
+
+document.querySelector('.js-row-to-be-injected').innerHTML = companiesHTML;
