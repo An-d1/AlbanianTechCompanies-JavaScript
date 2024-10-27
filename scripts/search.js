d563a09f9e0298f4bd69596f821f98636d6293b8
Andis <andisbara@gmail.com>
2024-10-27 14:37:01 +0100

    debounced the search bar to fix the lag proble,

--- scripts/search.js
+++ scripts/search.js
@@ -1,17 +1,28 @@
 import { companiesData } from "./data.js";
 
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
 function searchCompanies() {
   const userInput = document.querySelector('.js-searchInput');
   
-  userInput.addEventListener("input", e => {
+  userInput.addEventListener("input", debounce(e => {
     const value = e.target.value.toLowerCase();
 
     const searchResults = companiesData.filter(company => {
-      return company.titleCompany.toLowerCase().includes(value) || company.formattedAddress.toLowerCase().includes(value)// filter based on input
+      return (
+        company.titleCompany.toLowerCase().includes(value) || 
+        company.formattedAddress.toLowerCase().includes(value)
+      );
     });
 
-    displayResults(searchResults); // Display the filtered results
-  });
+    displayResults(searchResults);
+  }, 350)); // Adjust the delay as needed (in milliseconds)
 }
 
 // Function to display search results on the page
@@ -23,32 +34,26 @@ function displayResults(results) {
     results.forEach(company => {
       const companyElement =  `
       <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
-              <div class="flex-container">
-              <a class="websiteLink" href="${company.website}" target = _blank>
-    
-                <div class="mainImg">
-                  <img src="images/1721802899_0a08ccaa9a91933a0b20.jpg">
-                </div>
-    
-                <div class="title">
-                  "${company.titleCompany}"
-                </div>
-    
-                <div class="address">
-                  Address: ${company.formattedAddress}
-                </div>
-                </a>
-    
-                <a href="${company.link}" target = _blank>
-                <button class="location">
-                  open in google maps
-                </button>
-                </a>
-                
-              </div>
+        <div class="flex-container">
+          <a class="websiteLink" href="${company.website}" target="_blank">
+            <div class="mainImg">
+              <img src="images/1721802899_0a08ccaa9a91933a0b20.jpg">
             </div>
-        `;
-
+            <div class="title">
+              "${company.titleCompany}"
+            </div>
+            <div class="address">
+              Address: ${company.formattedAddress}
+            </div>
+          </a>
+          <a href="${company.link}" target="_blank">
+            <button class="location">
+              open in google maps
+            </button>
+          </a>
+        </div>
+      </div>
+      `;
       resultsDiv.innerHTML += companyElement;
     });
   } else {
@@ -56,9 +61,7 @@ function displayResults(results) {
     `<div class="container noSearchFound text-center">
       <div class="row d-flex justify-content-center">
         <div class="col-12">
-        <h1>
-          No Companies Found!
-        </h1>
+          <h1>No Companies Found!</h1>
         </div>   
       </div>
     </div>`;
