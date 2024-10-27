6ab983cb42aabc0ba8b2417af0ea7bba584a544a
Andis <andisbara@gmail.com>
2024-10-27 15:35:50 +0100

    removed classlist

--- scripts/search.js
+++ scripts/search.js
@@ -38,43 +38,45 @@ function displayResults(results) {
   const resultsDiv = document.querySelector('.js-row-to-be-injected');
   resultsDiv.innerHTML = ''; // Clear previous results
 
-  // Use a DocumentFragment to minimize reflows
   const fragment = document.createDocumentFragment();
 
   if (results.length > 0) {
     results.forEach(company => {
       const companyElement = document.createElement("div");
-      companyElement.classList.add("col-12", "col-sm-6", "col-md-6", "col-lg-4", "col-xl-3");
       companyElement.innerHTML = `
-        <div class="flex-container">
-          <a class="websiteLink" href="${company.website}" target="_blank">
-            <div class="mainImg">
-              <img src="images/1721802899_0a08ccaa9a91933a0b20.jpg" alt="${company.titleCompany}">
-            </div>
-            <div class="title">${company.titleCompany}</div>
-            <div class="address">Address: ${company.formattedAddress}</div>
-          </a>
-          <a href="${company.link}" target="_blank">
-            <button class="location">open in google maps</button>
-          </a>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+          <div class="flex-container">
+            <a class="websiteLink" href="${company.website}" target="_blank">
+              <div class="mainImg">
+                <img src="images/1721802899_0a08ccaa9a91933a0b20.jpg" alt="${company.titleCompany}">
+              </div>
+              <div class="title">${company.titleCompany}</div>
+              <div class="address">Address: ${company.formattedAddress}</div>
+            </a>
+            <a href="${company.link}" target="_blank">
+              <button class="location">open in google maps</button>
+            </a>
+          </div>
         </div>
       `;
       fragment.appendChild(companyElement);
     });
   } else {
     const noResultsElement = document.createElement("div");
-    noResultsElement.classList.add("container", "noSearchFound", "text-center");
     noResultsElement.innerHTML = `
-      <div class="row d-flex justify-content-center">
-        <div class="col-12">
-          <h1>No Companies Found!</h1>
-        </div>   
+      <div class="container noSearchFound text-center">
+        <div class="row d-flex justify-content-center">
+          <div class="col-12">
+            <h1>No Companies Found!</h1>
+          </div>   
+        </div>
       </div>
     `;
     fragment.appendChild(noResultsElement);
   }
 
-  resultsDiv.appendChild(fragment); // Update DOM once with the complete fragment
+  resultsDiv.appendChild(fragment);
 }
+
 
 searchCompanies(); // Initialize the search function
